7b8d468167875fb6009ee20b61238358fe74d700
Mario Drenski <marioodrenski@gmail.com>
2020-12-14 22:31:09 +0100

    initialize

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -49,7 +49,7 @@ class CountryController extends Controller
     public function show($id)
     {
         $country = Country::findOrFail($id);
-        dd($country);
+        return view('countries.show', compact('country'));
     }
 
     /**
--- resources/views/countries/index.blade.php
+++ resources/views/countries/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-
-<table class="table table-striped">
+<a href= "{{ route('countries.create') }}" class="btn btn-primary mt-5">Add</a>
+<table class="table table-striped mt-3">
   <thead>
     <tr>
       <th scope="col">Id</th>
@@ -16,8 +16,8 @@
     <td>{{$country->id}}</td>
     <td>{{$country->name}}</td>
     <td>
-      <a class="btn btn-outline-primary" href="">Details</a>
-      <a class="btn btn-outline-primary" href="">Edit</a>
+      <a class="btn btn-outline-primary" href="{{ route('countries.show', ['country' => $country->id]) }}">Details</a>
+      <a class="btn btn-outline-primary" href="{{ route('countries.edit', ['country' => $country->id]) }}">Edit</a>
     </tr>
 @endforeach
   </tbody>
